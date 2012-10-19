c1d10fa22c9fe4811aec86c2bcedd91989f73f0a
Hyungsuk Hong <aanoaa@cpan.org>
2012-10-19 04:00:36 +0000

    Import of AANOAA/Hubot-Scripts-Bundle-0.0.1 from CPAN.
    gitpan-cpan-distribution: Hubot-Scripts-Bundle
    gitpan-cpan-version:      0.0.1
    gitpan-cpan-path:         AANOAA/Hubot-Scripts-Bundle-0.0.1.tar.gz
    gitpan-cpan-author:       AANOAA
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,45 @@
+## p5-hubot-scripts ##
+
+### Installation ###
+
+    $ cpanm Hubot::Scripts::Bundle
+
+### Documentation ###
+
+just write a `pod` in source code
+
+```perl
+=head1 SYNOPSIS
+
+  hubot: usage here
+
+=head1 DESCRIPTION
+
+  this is blablabla
+
+=head1 CONFIGURATION
+
+=over
+
+=item ENV_VARS_TO_SET1
+
+=item ENV_VARS_TO_SET2
+
+=back
+
+=head1 AUTHOR
+
+Hyungsuk Hong <hshong@perl.kr>
+
+=cut
+```
+
+`cpan` or `cpanm` command will help dependency problem at install time
+
+### Make your own script for Dist::Zilla user ###
+
+make sure copy `misc/dzil/profiles/hubot` directory to `$HOME/.dzil/profiles`
+
+```bash
+$ dzil new -p hubot Hubot::Script::awesome
+```
---
+++ lib/Hubot/Scripts/redisBrain.pm
@@ -0,0 +1,54 @@
+package Hubot::Scripts::redisBrain;
+{
+  $Hubot::Scripts::redisBrain::VERSION = '0.0.1';
+}
+use strict;
+use warnings;
+use Redis;
+use JSON::XS;
+
+sub load {
+    my ( $class, $robot ) = @_;
+    my $coder = JSON::XS->new->convert_blessed;
+    my $redis = Redis->new(server => $ENV{REDIS_SERVER} || '127.0.0.1:6379');
+    print "connected to redis-server\n" if $ENV{DEBUG};
+    my $json = $redis->get('hubot:storage');
+    $robot->brain->mergeData(decode_json($json));
+    $robot->brain->on(
+        'save',
+        sub {
+            my ($e, $data) = @_;
+            my $json = $coder->encode($data);
+            $redis->set('hubot:storage', $json);
+        }
+    );
+    $robot->brain->on( 'close', sub { $redis->quit } );
+}
+
+1;
+
+=head1 NAME
+
+Hubot::Scripts::redisBrain
+
+=head1 SYNOPSIS
+
+=head1 CONFIGURATION
+
+=over
+
+=item REDIS_SERVER
+
+C<127.0.0.1:6379> is default to use.
+
+=back
+
+=head1 SEE ALSO
+
+L<https://github.com/github/hubot-scripts/blob/master/src/scripts/redis-brain.coffee>
+
+=head1 AUTHOR
+
+Hyungsuk Hong <hshong@perl.kr>
+
+=cut
